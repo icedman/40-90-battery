3f752e865ebfef21eaa78cca83703e4dd0ad1a7d
Marvin Sanchez <m4rvin2005@gmail.com>
2018-06-25 21:10:26 +0800

    the code

---
+++ 40-90-battery.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os
+import re
+
+# settings
+miio_device_id = '88038183'
+ceiling = 94
+floor = 40
+
+res = os.popen("pmset -g batt").read().replace('\n','')
+
+# level
+m = re.match(r'.*\s([0-9]{1,3})%', res)
+level = 0
+if m != None:
+    level = int(m.group(1))
+
+# state
+m = re.match(r'.*discharging', res)
+discharging = (m != None)
+
+print 'Battery Level: ' + str(level)
+print 'Discharging: ' + str(discharging)
+
+cmd = ''
+if level>ceiling and not discharging:
+    cmd = 'miio control ' + miio_device_id + ' power false'
+    print 'disconnect...'
+
+if level<floor and discharging:
+    cmd = 'miio control ' + miio_device_id + ' power true'
+    print 'connect...'
+
+if cmd != '':
+    os.popen(cmd)
+else:
+    print 'do nothing.'
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # 40-90-battery
 Saves laptop battery by disconnecting charger power at 90, reconnecting at 40.
+
+[Xiaomi Smart Power Socket](https://www.aliexpress.com/item/Original-Xiaomi-MiJia-Mi-Smart-Power-Socket-Plug-Basic-Wireless-WiFi-APP-Remote-Control-Timer-Switch/32801060790.html)
+
+require npm miio
+``npm install -g miio``
+
+crontab -e
+``45 * * * * /usr/local/bin/40-90-battery.py``
